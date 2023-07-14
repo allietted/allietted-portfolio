38863250cabd4c97fd5fc77119946fb875d68605
Alliette Dortelus <allietted@gmail.com>
2023-07-14 11:41:51 -0400

    added portfolio

---
+++ src/layout/Portfoilo/PortfolioPanel.jsx
@@ -0,0 +1,20 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import portfolioData from '../../data/portfolio.json';
+import { useState } from 'react';
+
+
+export default function PortfolioPanel({data:{title,content, website,cover}}) {
+
+    const [showModal, setShowModal] = useState(false)
+    const toggleModalShow = () => setShowModal(!showModal)
+
+    const imageUri = process.env.PUBLIC_URL + '/portfoilo'
+
+    return(
+        <Col xs={11} md={6} lg={4} xl={3}>
+            <Image src={image}/>
+        </Col>
+    )
+
+
+}
