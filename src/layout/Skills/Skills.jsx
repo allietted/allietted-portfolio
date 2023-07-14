56566cb086c2a9ed28a63fa32e104a0343ae1029
Alliette Dortelus <allietted@gmail.com>
2023-07-13 22:33:38 -0400

    added technical skills

---
+++ src/layout/Skills/Skills.jsx
@@ -0,0 +1,28 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import Panel from './Panel';
+import icon from '../../data/icon.json';
+
+
+export default function Skills() {
+
+    return(
+        <Container id='skills'>
+            <h1 className='text-center'>Technical Proficiencies</h1>
+            <Row className='mt-3 justify-content-center'>
+                <Col md={7} className='text-center'>
+                 <h3 className='mb-4 text-center'>I'm not a coder; I'm a code, man!!!!!</h3>
+                
+                {
+                    icon.map((element) =>{
+                        return(
+                            <Panel key={element.id}
+                            name={element.name}/>
+                        )
+                    })
+                }
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
