7542fa144ba819777be8d3214b2200fad374c6ec
Alliette Dortelus <allietted@gmail.com>
2023-07-15 19:42:57 -0400

    "contact complete"

--- src/layout/Contact/Contact.jsx
+++ src/layout/Contact/Contact.jsx
@@ -1,9 +1,59 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify"
 
-export default function Contact() {
+export default function ContactModal() {
+  const [formData, setFormData] = useState({ name: "", email: "", message: "" });
 
-    const[formData, setFormData] = useState({})
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch("https://contact-iv.web.app/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP status ${response.status}`);
+      }
+      setFormData({ name: "", email: "", message: "" });
+      toast.success("Success: E-mail has been sent!");
+    } catch (error) {
+      toast.error(`Error: Email hasn't been sent. ${error.message}`);
+    }
+  };
+  
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
+  return (
+    <>
+
+          <Form  id="contact" onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" name="name" value={formData.name} onChange={handleInputChange} required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email" name="email" value={formData.email} onChange={handleInputChange} required />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Enter your message" name="message" value={formData.message} onChange={handleInputChange} required />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+    </>
+  );
 }
